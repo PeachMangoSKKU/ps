599f340f8d0b398981f633c4ccc8f995683dbce7
juwkim <gimjuwon-ui-MacBookPro.local>
2023-12-28 22:32:52 +0900

    Feat: add validation check

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 from fastapi import FastAPI
+from fastapi import Query, Path
 from fastapi import File, UploadFile
-from fastapi.responses import FileResponse
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
+from typing import Annotated
 
 import os
-import shutil
+import aiofiles
 
 app = FastAPI()
 
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
+async def create_upload_file(
+    file: UploadFile = File(...)
+):
     if not os.path.exists("uploaded_files"):
         os.mkdir("uploaded_files")
     file_location = f"uploaded_files/{file.filename}"
-    with open(file_location, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+    async with aiofiles.open(file_location, 'wb') as out_file:
+        content = await file.read()
+        await out_file.write(content)
     return {"info": f"file '{file.filename}' saved at '{file_location}'"}
 
 @app.get("/downloadfile/{file_name}")
-async def download_file(file_name: str):
-    # 파일 위치 지정
+async def download_file(
+    file_name: str,
+    q: Annotated[
+        str,
+        Query(
+            alias="q",
+            title="q",
+            description="q",
+            min_length=1,
+            max_length=50,
+            pattern="^[\w\-. ]+$",
+        ),
+    ] = ...
+):
+    if not os.path.exists("uploaded_files"):
+        os.mkdir("uploaded_files")
     file_path = f"uploaded_files/{file_name}"
     if os.path.exists(file_path):
         return FileResponse(path=file_path, filename=file_name)
